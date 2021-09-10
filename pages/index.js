06e99fdde856a750fa5c966c64c42362a4dfe880
Ethan McCoy <ethanmccoy@gmail.com>
2021-09-10 14:38:12 -0700

    me too test git commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import { Container } from 'react-bootstrap'
-import PageLayout from '../components/PageLayout'
+import { Container } from "react-bootstrap";
+import PageLayout from "../components/PageLayout";
+
+var sam = 'sexy'
 
 export default function Home() {
   return (
     <Container>
       <div>
-        <h1>Hello</h1>
+          <h1>I just pooped myself</h1>          
+          <h1>Hello</h1>
       </div>
     </Container>
-  )
+  );
 }
